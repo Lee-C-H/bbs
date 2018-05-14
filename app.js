620632104c73cdf59d97faea11209b4389a24e1e
Lee-C-H <brisgi2@naver.com>
2018-05-14 20:32:53 +0900

    초반

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 var express = require('express');
-
+var path = require('path');
 var app = express();
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://test:ckdghks1!@ds123490.mlab.com:23490/bbs');
+var db = mongoose.connection;
+db.once('open', function(){
+    console.log("Connected to mongod server");
+});
+db.on('error', console.error);
+
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
 app.get('/',function(req,res){
-    res.send('Hello World!');
+    res.render('test');
 });
 
 app.listen(3000, function(){
